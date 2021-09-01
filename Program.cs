e2300824a6132a1384a1c746964c4c320e310336
Janabai Tukaram Tidke <janatidke2407@gmail.com>
2021-09-01 19:25:29 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace TernaryOperatorDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int balance = 10000, withdrawal;
+            Console.Write("Enter Withdrawal Amount: ");
+            withdrawal = Convert.ToInt32(Console.ReadLine());
+            balance = withdrawal > balance ? balance : balance - withdrawal;
+            Console.WriteLine("Balance= " + balance);
+
+
+        }
+    }
+}
